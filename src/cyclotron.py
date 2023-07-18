cf628ec2aec3c37f6f73baa60e6d555ae211f141
Matheus Diógenes <mathdiogenes@gmail.com>
2023-07-18 15:02:07 -0300

    feat: neutron function

--- src/cyclotron.py
+++ src/cyclotron.py
@@ -1,14 +1,17 @@
 from without_particle import without_particle
 from electron import electron
+from neutron import neutron
 
 
 def circulate_cyclotron(matrix, particle=None):
     if particle == "e":
         return electron(matrix)
+    elif particle == "n":
+        return neutron(matrix)
     elif particle is None:
         return without_particle(matrix)
     else:
         return "Invalid particle!"
 
 
-print(circulate_cyclotron(7, "e"))
+print(circulate_cyclotron(7, "n"))
--- src/neutron.py
+++ src/neutron.py
@@ -1,12 +1,11 @@
 import numpy as np
 
 
-def electron(matrix):
-    accelerating = [["e"] * matrix]
+def neutron(matrix):
+    accelerating = [["n"] * matrix]
 
     for _ in range(0, matrix - 1):
-        values = [1] * (matrix - 1)
-        values.append("e")
+        values = [1] * matrix
         accelerating = np.append(accelerating, [values], axis=0)
 
     return accelerating
